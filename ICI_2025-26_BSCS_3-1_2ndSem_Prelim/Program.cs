c4a79e7d414e42934172188b00a671d411bdd080
Jesse Castro <jcastro@jaisoftawre.net>
2026-01-22 03:44:18 +0800

    Add project files.

---
+++ ICI_2025-26_BSCS_3-1_2ndSem_Prelim/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
