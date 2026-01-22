963ad07b91a5fe61fd5dc4dcd2746bf3782348b6
oxygn02 <jldeasis02@gmail.com>
2026-01-22 09:38:42 +0000

    Update SeondPart.cs

--- ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
+++ ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ICI_2025_26_BSCS_3_1_2ndSem_Prelim.SeondPart;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
 {
@@ -36,7 +38,59 @@ namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
         {
             Console.WriteLine("SECOND PART");
 
-            //Put your code here
+            
+                Student student1 = new Student();
+                Student student2 = new Student();
+                Student student3 = new Student();
+
+
+                student1.StudentNumber = "2023-1396-IC";
+                student1.Name = "Jhonlloyd";
+                student1.Course = "BSCS";
+                student1.Year = 3;
+                student1.Section = 2;
+                student1.FavoriteFilipinoHero = "Jose Rizal";
+
+
+
+                student2.StudentNumber = "2024-1245-IC";
+                student2.Name = "Nhess";
+                student2.Course = "BSCS";
+                student2.Year = 3;
+                student2.Section = 2;
+                student2.FavoriteFilipinoHero = "Andres";
+
+                Console.WriteLine("student1 Name: " + student1.Name);
+                
+                Console.WriteLine("student1 FavoriteFilipinoHero: " + student1.FavoriteFilipinoHero);
+                
+
+
+                student2.Name = "Nhess";
+                student2.FavoriteFilipinoHero = "Andres";
+
+                Console.WriteLine("student1 Name: " + student1.Name);
+                Console.WriteLine("student2.Name: " + student2.Name);
+                Console.WriteLine("student2.FavoriteFilipinoHero: " + student2.FavoriteFilipinoHero);
+
+                Console.ReadKey();
+            
         }
+        public struct Student
+        {
+
+            public string StudentNumber;
+            public string Name;
+            public string Course;
+            public int Year;
+            public int Section;
+            public string FavoriteFilipinoHero;
+            
+
+
+
+        }
+
     }
 }
+    
