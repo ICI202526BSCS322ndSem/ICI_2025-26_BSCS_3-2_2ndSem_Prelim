ca524c5d7fdee12c9deb6eb0e9feb130668e41b9
melissadecastro071-bit <melissadecastro071@gmail.com>
2026-01-22 01:35:07 -0800

    Update SeondPart.cs

--- ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
+++ ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,9 +36,24 @@ namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
     {
         public void Build()
         {
-            Console.WriteLine("SECOND PART");
 
-            //Put your code here
+            Console.WriteLine("STUDENT NUMBER");
+            Console.WriteLine("2023-1001-IC");
+
+            Console.WriteLine("STUDENT NAME");
+            Console.WriteLine("MELISSA A. DE CASTRO");
+
+            Console.WriteLine("COURSE");
+            Console.WriteLine("BACHELOR OF SCIENCE IN COMPUTER SCIENCE");
+
+            Console.WriteLine("YEAR LEVEL");
+            Console.WriteLine("3");
+
+            Console.WriteLine("FAVORITE FILIPINO HERO");
+            Console.WriteLine("ANDRES BONIFACIO");
         }
     }
 }
+
+
+
