a2ffe09ee6d3e227076340bd5381b1ea844ee6aa
Carl-YingYang <carlmickynieva@gmail.com>
2026-01-22 17:34:30 +0800

    initial commit

--- ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
+++ ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static ICI_2025_26_BSCS_3_1_2ndSem_Prelim.SeondPart;
 
 namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
 {
@@ -37,6 +39,44 @@ namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
             Console.WriteLine("SECOND PART");
 
             //Put your code here
+            Student student1 = new Student();
+            Student student2 = new Student();
+            Student student3 = new Student();
+
+            student1.StudentNumber = "0723";
+            student1.StudentName = "Carl";
+            student1.Course = "Bscs";
+            student1.YearLevel = 3;
+            student1.Section = 2;
+            student1.FavoriteFilipinoHero = "Rizal";
+
+            student2 = student1;
+
+            Console.WriteLine("Student 1 Details: " + student1.StudentNumber);
+            Console.WriteLine("Student 1 Details: " + student1.Course);
+            Console.WriteLine("Student 1 Details: " + student1.StudentName);
+            Console.WriteLine("");
+
+            student2.StudentNumber = "0724";
+            student2.StudentName = "Nieva";
+            student2.Course = "Bscs";
+
+            Console.WriteLine("Student 2 Details: " + student2.StudentNumber);
+            Console.WriteLine("Student 2 Details: " + student2.Course);
+            Console.WriteLine("Student 2 Details: " + student2.StudentName);
+            Console.WriteLine("");
+
+            Console.WriteLine("Student 1 Details: " + student1.StudentNumber);
+            Console.WriteLine("Student 1 Details: " + student2.StudentNumber);
+        }
+        public class Student
+        {
+            public string StudentNumber ;
+            public string StudentName;
+            public string Course;
+            public int YearLevel;
+            public int Section;
+            public string FavoriteFilipinoHero;
         }
     }
 }
