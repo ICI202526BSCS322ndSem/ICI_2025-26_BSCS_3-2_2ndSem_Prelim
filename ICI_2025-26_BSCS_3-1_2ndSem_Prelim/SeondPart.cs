d53efe8e20e3d9fa0ab9dbf2b94c335dbee7bf1c
Uncannyjrevs <ragajervee@gmail.com>
2026-01-22 17:30:20 +0800

    Add Student class and instance creation
    Implemented Student class with properties and created instances to display student information.

--- ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
+++ ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
@@ -1,42 +1,58 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
 {
-    /*
-    CREATE A STUDENT CLASS WITH THE FOLLOWING PROPERTIES
 
-    STUDENT NUMBER (STRING)
-    STUDENT NAME (STRING)
-    COURSE (STRING)
-    YEAR LEVEL (INTEGER)
-    SECTION (INTEGER)
-    FAVORITE FILIPINO HERO (STRING)
-
-
-    create an instance of Student and name it student1;
-    assign values to each property of student1;
-    print the values of each property to the console in a readable format.
-
-    create another instance of Student and name it student2;
-    assign student1 to student2;
-    change the Student Name and Favorite Filipino Hero properties of student2 to different values;
-    
-    print the values of each property of student1 to the console again to observe any changes.
-    print the values of each property of student2 to the console to see the updated values.
-    
-    */
-
+    public class Student
+    {
+        public int Number;
+        public string Name;
+        public string Course;
+        public int YEAR_LEVEL;
+        public int Section;
+        public string Favorite_Filipino_Hero;
+    }
     internal class SeondPart
     {
         public void Build()
         {
-            Console.WriteLine("SECOND PART");
+            Console.WriteLine("Seond Part");
+            Console.WriteLine("");
 
-            //Put your code here
+
+            Student student1 = new Student();
+            student1.Number = 2023;
+            student1.Name = "raga, jervee angelo";
+            student1.Course = "BSCS";
+            student1.YEAR_LEVEL = 3;
+            student1.Section = 2;
+            student1.Favorite_Filipino_Hero = "Heneral Antonio Luna";
+
+            Console.WriteLine("My student id is " + student1.Number + " and my name is " + student1.Name + " I am currently part of " + student1.Course + student1.YEAR_LEVEL + student1.Section + " And my favorite Filipino hero is " + student1.Favorite_Filipino_Hero);
+            Console.WriteLine("");
+
+            Student student2 = new Student();
+            student2.Number = 2024;
+            student2.Name = "tabudlo, Jenard Phil";
+            student2.Course = "BSCS";
+            student2.YEAR_LEVEL = 3;
+            student2.Section = 2;
+            student2.Favorite_Filipino_Hero = "Heneral Antonio Luna";
+
+            Console.WriteLine("My student id is " + student2.Number + " and my name is " + student2.Name + " I am currently part of " + student2.Course + student2.YEAR_LEVEL + student2.Section + " And my favorite Filipino hero is " + student2.Favorite_Filipino_Hero);
+            Console.WriteLine("");
+
+
+
+
+
+
+
         }
     }
 }
