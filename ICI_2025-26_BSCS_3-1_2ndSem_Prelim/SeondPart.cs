7b129c5b78eadfc4c93701397529db46fe5f9d81
Kissette Tiffany Panganiban <kissettep@gmail.com>
2026-01-22 17:33:41 +0800

    Fix comments and class name in SeondPart.cs
    Updated comments and corrected class name from 'SeondPart' to 'SecondPart'.

--- ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
+++ ICI_2025-26_BSCS_3-1_2ndSem_Prelim/SeondPart.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,37 +6,52 @@ using System.Threading.Tasks;
 
 namespace ICI_2025_26_BSCS_3_1_2ndSem_Prelim
 {
-    /*
-    CREATE A STUDENT CLASS WITH THE FOLLOWING PROPERTIES
+    //    /*
+    //    CREATE A STUDENT CLASS WITH THE FOLLOWING PROPERTIES
 
-    STUDENT NUMBER (STRING)
-    STUDENT NAME (STRING)
-    COURSE (STRING)
-    YEAR LEVEL (INTEGER)
-    SECTION (INTEGER)
-    FAVORITE FILIPINO HERO (STRING)
+    //    STUDENT NUMBER (STRING)
+    //    STUDENT NAME (STRING)
+    //    COURSE (STRING)
+    //    YEAR LEVEL (INTEGER)
+    //    SECTION (INTEGER)
+    //    FAVORITE FILIPINO HERO (STRING)
 
 
-    create an instance of Student and name it student1;
-    assign values to each property of student1;
-    print the values of each property to the console in a readable format.
+    //    create an instance of Student and name it student1;
+    //    assign values to each property of student1;
+    //    print the values of each property to the console in a readable format.
 
-    create another instance of Student and name it student2;
-    assign student1 to student2;
-    change the Student Name and Favorite Filipino Hero properties of student2 to different values;
-    
-    print the values of each property of student1 to the console again to observe any changes.
-    print the values of each property of student2 to the console to see the updated values.
-    
-    */
+    //    create another instance of Student and name it student2;
+    //    assign student1 to student2;
+    //    change the Student Name and Favorite Filipino Hero properties of student2 to different values;
 
-    internal class SeondPart
+    //    print the values of each property of student1 to the console again to observe any changes.
+    //    print the values of each property of student2 to the console to see the updated values.
+
+    //    */
+
+    class SecondPart
     {
         public void Build()
         {
-            Console.WriteLine("SECOND PART");
 
-            //Put your code here
+            Console.WriteLine("STUDENT NUMBER");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("STUDENT NAME");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("STUDENT NUMBER");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("STUDENT NUMBER");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("STUDENT NUMBER");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("FAVORITE FILIPINO HERO");
+            Console.WriteLine(" ");
         }
     }
 }
